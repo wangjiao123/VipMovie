0bb4c9ad56b54ba285ba8ddc416013631201c986
xx-xiaobai <397667137@qq.com>
2017-12-08 16:13:08 +0800

    闫鑫

--- app/src/main/java/com/vip/movie/MyApp.java
+++ app/src/main/java/com/vip/movie/MyApp.java
@@ -22,6 +22,7 @@ public class MyApp extends Application {
 //    }
 
     {
+        //sb
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
         PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
