bab6e47052480a699c4c95b3678fa3fdebd93b67
xx-xiaobai <397667137@qq.com>
2017-12-09 16:08:22 +0800

    11

--- app/src/main/java/com/vip/movie/MyApp.java
+++ app/src/main/java/com/vip/movie/MyApp.java
@@ -23,12 +23,7 @@ public class MyApp extends Application {
 
 
 //456456465
-//        1231321256
-//        123132125
 
-
-//        1231211232
-//        123132125
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
         PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
