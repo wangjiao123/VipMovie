502f2d40cdaf4b1c7397dba25abbee4e51c1e262
xx-xiaobai <397667137@qq.com>
2017-12-09 15:38:40 +0800

    yanxin

--- app/src/main/java/com/vip/movie/MyApp.java
+++ app/src/main/java/com/vip/movie/MyApp.java
@@ -20,7 +20,7 @@ import java.util.Set;
 
 public class MyApp extends Application {
     {
-//        222
+//        22233
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
         PlatformConfig.setWeixin("wx967daebe835fbeac", "5bb696d9ccd75a38c8a0bfe0675559b3");
         PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
